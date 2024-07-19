9c6bd93538cb7b36c216edf4d4a7c421aa3cce5c
vktrung <trung040102@gmail.com>
2024-07-19 11:08:30 +0700

    filter detail

--- Library/Pages/Books/Detail.cshtml.cs
+++ Library/Pages/Books/Detail.cshtml.cs
@@ -1,31 +1,38 @@
 using LibraryCore.Models;
 using LibraryCore.UnitOfWork;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Hosting;
 
 namespace Library.Pages
 {
-    public class DetailModel : PageModel
-    {
-        public LibraryCore.Models.Book Book { get; set; }
-        public IList<LibraryCore.Models.Book> BooksByCategory { get; set; }
-        private readonly IUnitOfWork _unitOfWork;
-        public DetailModel(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-        public void OnGet(int id)
-        {
-            Book = _unitOfWork.BookRepository.Find(id);
-            if (Book != null)
-            {
-                BooksByCategory = _unitOfWork.BookRepository.GetAll().Where(x => x.CategoryId == Book.CategoryId
-                                                                            && x.BookId != Book.BookId
-                                                                            && x.Status.Value).OrderByDescending(x => x.BookId).ToList();
-                ViewData["Title"] = Book.Title;
-            }
+	public class DetailModel : PageModel
+	{
+		public LibraryCore.Models.Book Book { get; set; }
+		public IList<LibraryCore.Models.Book> BooksByCategory { get; set; }
+		private readonly IUnitOfWork _unitOfWork;
+		public LibraryCore.Models.Category Categories { get; set; }
+		public LibraryCore.Models.Author Authors { get; set; }
+		public IList<Category> ListCategories { get; set; }
+		public DetailModel(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public void OnGet(int id)
+		{
+			Book = _unitOfWork.BookRepository.Find(id);
+			Categories = _unitOfWork.CategoryRepository.Find(Book.CategoryId);
+			Authors = _unitOfWork.AuthorRepository.Find(Book.AuthorId);
+			ListCategories = _unitOfWork.CategoryRepository.GetAll();
 
-        }
-    }
+			if (Book != null)
+			{
+				//BooksByCategory = _unitOfWork.BookRepository.GetAll().Where(x => x.CategoryId == Book.CategoryId
+				//															&& x.BookId != Book.BookId
+				//															&& x.Status.Value).OrderByDescending(x => x.BookId).ToList();
+
+				BooksByCategory = _unitOfWork.BookRepository.GetAll().Where(x => x.AuthorId == Authors.AuthorId).ToList();
+				ViewData["Title"] = Book.Title;
+			}
+
+		}
+	}
 }
